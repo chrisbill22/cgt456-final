928b4b60dc151a8b826b0b302607c564b004b2f6
CB MacBook Pro <orangedog222@gmail.com>
2014-05-02 20:40:45 -0400

    Style and structure changes

--- index.php
+++ index.php
@@ -10,42 +10,30 @@
    
     <script type="text/javascript" src="jqueryUI/js/jquery-1.10.2.js"></script>
     <script type="text/javascript" src="jqueryUI/js/jquery-ui-1.10.4.custom.min.js"></script>
+    <script type="text/javascript" src="actions.js"></script>
 	<script type="text/javascript" src="loader/loader.js"></script>
 	<script type="text/javascript" src="gdrive/gdrive.js"></script>
 	<script type="text/javascript" src="facebook/facebook.js"></script>
-	
+	<script type="text/javascript" src="dropbox/dropbox.js"></script>	
 	<script type="text/javascript">
 		var gdID = '<?php if(!empty($_SESSION['gdID'])){ echo $_SESSION['gdID']; } ?>';
+		var dbID = '<?php if(!empty($_SESSION['dbID'])){ echo $_SESSION['dbID']; } ?>';
 		//var accessObj = eval(<?php if(!empty($_SESSION['gdID'])){ echo $_SESSION['gdID']; } ?>);
 		gDriveSubdir = "gdrive/";
+		dBoxSubdir = "dropbox/";
 		google_folderDiv = "#googleFolders .folders";
 		google_fileDiv = "#googleFiles";
+		dropbox_folderDiv = "#dropboxFolders .folders";
+		dropbox_fileDiv = "#dropboxFiles";
 	</script>
     
 </head>
 <body>
-	<!-- LOGIN POPUP -->
-	<div id="mainLogin">
-		<div id="mainLogin_loading">
-			<center>
-				<br /><br />
-				Please Wait...
-			</center>
-		</div>
-		<div id="mainLogin_login" style="display: none;">
-			<center>
-				<small>Login with your Facebook ID</small>
-				<br /><br />
-				<fb:login-button scope="public_profile,email" size="xlarge" onlogin="checkLoginState();">
-				</fb:login-button>
-			</center>
-		</div>
-	</div>
 	
     <div id="header">
         <img src='images/NavBG.png' />
-        <a href=""><img class="nav-AddCloudDevice" src="images/AddCloudDevice.png"></a>
-        <img class="nav-AddFolder" src="images/AddFolder.png">
+        <a href="#" onclick="openFileUpload();"><img class="nav-AddCloudDevice" src="images/AddCloudDevice.png"></a>
+        <a href="#" onclick="openNewFolderPopup();"><img class="nav-AddFolder" src="images/AddFolder.png" /></a>
         <a href="#" class="nav-Logout">Logout</a>
         
         <img id="logo" src="images/CumulusLogo.png" alt="Cumulus Drive" />
@@ -96,7 +84,8 @@
                 <!-- Column 2 start -->
 				
 				<div id="dropboxAdd" style="display: none;">
-					<a href="https://cgt456.genyapps.com/dropbox/pages/login.php">
+					<!--<a href="https://cgt456.genyapps.com/dropbox/pages/login.php">-->
+					<a href="dropbox/pages/login.php">
 		                <img class="CloudNotConnected" src="images/dropboxlogo.png" width="40" border="0" height="37" alt="DropBox Logo" />
 		                <h3 class="CloudNotConnected">Connect your account!</h3>
 	                </a>
@@ -111,20 +100,21 @@
                 	<img class="CloudAccount" src="images/googledrivelogo.png" width="35" border="0" height="31" alt="Google Drive Logo" />
 		            <h3>Your Drive</h3>
 		            <div class="folders">
-	                	<button onclick="getFiles('')">Get Files</button>
+	                	<button onclick="gd_getFiles('')">Get Files</button>
 	                </div>
                 </div>
                 <div id="dropboxFolders" style="display: none;">
                 	<img class="CloudNotConnected" src="images/dropboxlogo.png" width="40" border="0" height="37" alt="DropBox Logo" />
 	                <h3 class="CloudNotConnected">Your Dropbox</h3>
 	                <div class="folders">
-	                	<ul>
+	                	<button onclick="db_getFiles('')">Get Files</button>
+	                	<!--<ul>
 		 					<li><img class="image" src="images/SharedFolder.png" width="30" border="0" height="30" alt="Shared Folder" />Ben and Jess</li>
 		                    <li><img class="image" src="images/DefaultFolder.png" width="30" border="0" height="30" alt="Folder" />CGT</li>
 		                    <li><img class="image" src="images/DefaultFolder.png" width="30" border="0" height="30" alt="Folder" />EBOOK</li>
 		                    <li><img class="image" src="images/DefaultFolder.png" width="30" border="0" height="30" alt="Folder" />Spring Break</li>
 		                    <li><img class="image" src="images/SharedFolder.png" width="30" border="0" height="30" alt="Folder" />Treasurer</li>
-		                </ul>
+		                </ul>-->
 	                </div>
                 </div>
                 
@@ -134,7 +124,45 @@
     </div>
     <div id="footer">
         <p>CGT 456 Project 3 By: Chris Bill, Jessica Theuerl, Josh Hewitt, and Sean Heckman-Davis<br /></p>
-
     </div>
+    
+    <!------------------->
+    <!------POPUPS------->
+    <!------------------->
+    
+    <!-- LOGIN POPUP -->
+	<div id="mainLogin">
+		<div id="mainLogin_loading">
+			<center>
+				<br /><br />
+				Please Wait...
+			</center>
+		</div>
+		<div id="mainLogin_login" style="display: none;">
+			<center>
+				<small>Login with your Facebook ID</small>
+				<br /><br />
+				<fb:login-button scope="public_profile,email" size="xlarge" onlogin="checkLoginState();">
+				</fb:login-button>
+			</center>
+		</div>
+	</div>
+    
+    <!-- RENAME POPUP -->
+    <div id="renamePopup" style="display: none;">New Name: <br /><input type='text' /></div>
+    
+    <!-- NEW FOLDER POPUP -->
+    <div id="newFolderPopup" style="display: none;">Folder Name: <br /><input type='text' /></div>
+    
+    <!-- UPLOAD FILE POPUP -->
+    <div id="newFilePopup" style="display: none;">
+    	<form enctype="multipart/form-data" id="uploadFileFormd">
+	    	<input type="file" id="uploadFile" name="gdriveFile" />
+	    	<input type='button' id="uploadFileBtn" value="Upload!" onclick="gd_uploadFile('uploadFileFormd')">
+			<input type="hidden" name="folderID" id="data_folderID" />
+			<input type="hidden" name="access_token" id="gdriveAuth"/>
+		</form>
+	</div>
+    
 </body>
 </html>
